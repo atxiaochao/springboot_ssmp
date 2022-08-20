a61fbb3dba205515ce9afbef42b142b70c20381e
xiaochao <xiaochao@xiaochao.com>
2022-08-20 20:07:13 +0800

    hot-fix commit1

---
+++ src/test/java/com/xiaochao/dao/testDemo.java
@@ -0,0 +1,9 @@
+package com.xiaochao.dao;
+
+import net.minidev.json.JSONUtil;
+
+public class testDemo {
+    public static void main(String[] args) {
+        System.out.println("无敌无敌单位我的");
+    }
+}
